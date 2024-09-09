5a21cc5adac71d982aea384fa913180824979c69
Emerson Costa <emersonjscosta@gmail.com>
2024-09-09 19:44:48 -0300

    Feat: implemented datas of front

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log(data);
+let section = document.getElementById("resultados-pesquisa");
+let results = "";
+for (let data of datas ){
+    results+= `
+                            <div class="item-resultado">
+                                <h2>
+                                    <a href="#" target="_blank">${data.title}</a>
+                                </h2>
+                                <p class="descricao-meta">${data.description}</p>
+                                <p>Gênero: ${data.gender}</p>
+                                <a href=${data.link} target="_blank">Mais Informações</a>
+                            </div>
+    `
+}
+section.innerHTML = results;
---
+++ datas.js
@@ -0,0 +1,22 @@
+let datas = [
+    {
+      title: "O Poderoso Chefão",
+      description: "A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos.",
+      gender: "Drama, Crime",
+      link: "https://www.imdb.com/title/tt0068646/"
+    },
+    {
+      title: "O Senhor dos Anéis: A Sociedade do Anel",
+      description: "Um hobbit se junta a um mago e dois elfos em uma jornada para destruir um anel maligno.",
+      gender: "Fantasia, Aventura",
+      link: "https://www.imdb.com/title/tt0120737/"
+    },
+    {
+      title: "Interestelar",
+      description: "Um grupo de astronautas embarca em uma missão para encontrar um novo lar para a humanidade.",
+      gender: "Ficção Científica",
+      link: "https://www.imdb.com/title/tt0816692/"
+    }
+  ];
+
+
