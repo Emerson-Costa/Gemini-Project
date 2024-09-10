12c2ce3f31b7d57db57f34bfb88be5c2aaa5b75e
Emerson Costa <emersonjscosta@gmail.com>
2024-09-09 22:38:47 -0300

    Feat: finally project

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 let search = () => {
     // Seleciona a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa"); 
+    let section = document.getElementById("resultados-pesquisa");
+    
+    // Seleciona o valor que foi inserido no campo de pesquisa
+    let searchField = document.getElementById("search-field").value; 
+    searchField = searchField.toLowerCase();
+    
+    // Se o campo de pesquisa estiver vazio, não retorna nada.
+    if(searchField == ""){
+        section.innerHTML = "<p>Nenhum resultado foi encontrado!</p>"
+        return;
+    }
 
     // Inicializa uma string vazia para armazenar o HTML dos resultados
     let results = ""; 
+    let title = "";
+    let description = "";
+    let tags = "";
 
     // Itera sobre cada item do array de dados
     for (let data of datas) { 
-        // Constrói o HTML para cada item, formatando os dados
-        results += `
+        title = data.title.toLowerCase();
+        description = data.description.toLowerCase();
+        tags = data.tags.toLowerCase();
+        if(title.includes(searchField) || description.includes(searchField) || tags.includes(searchField)){
+            // Constrói o HTML para cada item, formatando os dados
+            results += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${data.title}</a>
@@ -17,10 +34,18 @@ let search = () => {
                 <p>Gênero: ${data.gender}</p>
                 <a href=${data.link} target="_blank">Mais Informações</a>
             </div>
-        `;
+            `;
+        }
     }
 
-    // Atribui o HTML gerado para a seção, renderizando os resultados na página
-    section.innerHTML = results;
+     // Atribui o HTML gerado para a seção, renderizando os resultados na página
+    if(!results){
+        section.innerHTML = "<p>Nenhum resultado foi encontrado, tente novamente.</p>"
+    }else{
+        section.innerHTML = results;
+    }
+
+   
+   
 }
 
--- datas.js
+++ datas.js
@@ -3,19 +3,29 @@ let datas = [
       title: "O Poderoso Chefão",
       description: "A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos.",
       gender: "Drama, Crime",
-      link: "https://www.imdb.com/title/tt0068646/"
+      link: "https://www.imdb.com/title/tt0068646/",
+      tags: "chefao chefao o poderoso chefao"
     },
     {
       title: "O Senhor dos Anéis: A Sociedade do Anel",
       description: "Um hobbit se junta a um mago e dois elfos em uma jornada para destruir um anel maligno.",
       gender: "Fantasia, Aventura",
-      link: "https://www.imdb.com/title/tt0120737/"
+      link: "https://www.imdb.com/title/tt0120737/",
+      tags: "aneis anel"
     },
     {
       title: "Interestelar",
       description: "Um grupo de astronautas embarca em uma missão para encontrar um novo lar para a humanidade.",
       gender: "Ficção Científica",
-      link: "https://www.imdb.com/title/tt0816692/"
+      link: "https://www.imdb.com/title/tt0816692/",
+      tags: "interstelar interestrelar"
+    },
+    {
+      title: "O Poderoso Chefão 2",
+      description: "A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos.",
+      gender: "Drama, Crime",
+      link: "https://www.imdb.com/title/tt0068646/",
+      tags: "chefao cefao o poderoso chefao"
     }
   ];
 
