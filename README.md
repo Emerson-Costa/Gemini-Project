2dc37356b5bba380a205c982ddedfd6f62fe7e2e
Emerson Costa <emersonjscosta@gmail.com>
2024-09-10 15:00:55 -0300

    Feat: Creat README.md and alter tags

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Aplicação para realizar busca de filmes</h1>
+<div style="width=10px" >
+    <img src="./resources/presentation.png"></img>
+<div>
+<p>Este projeto foi desenvolvido durante a imersão da Alura, que propôs um desafio para criar uma aplicação capaz de realizar buscas.</p>
+
+<p>A base de dados do projeto foi gerada pela IA generativa Google Gemini, para realizar esses testes </p>
+
+<h2>Como Utilizar?</h2>
+<ol>
+    <li>Abra o arquivo index.html do projeto</li>
+    <li>Escreva "Listar todos", para vizualizar todos os filmes que estão registrados na base de dados.</li>
+    <img src="./resources/step-2.png"></img>
+    <li>Pesquise pelo menos um destes filmes ou o gênero dos mesmos.</li>
+    <img src="./resources/step-3.png"></img>
+</ol>
+
+
+
--- datas.js
+++ datas.js
@@ -2,7 +2,7 @@ let datas = [
     {
       title: "O Poderoso Chefão",
       description: "A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos.",
-      gender: "Drama, Crime",
+      gender: "Drama Crime listar todos",
       link: "https://www.imdb.com/title/tt0068646/",
       tags: "chefao chefao o poderoso chefao"
     },
@@ -11,21 +11,21 @@ let datas = [
       description: "Um hobbit se junta a um mago e dois elfos em uma jornada para destruir um anel maligno.",
       gender: "Fantasia, Aventura",
       link: "https://www.imdb.com/title/tt0120737/",
-      tags: "aneis anel"
+      tags: "aneis anel listar todos"
     },
     {
       title: "Interestelar",
       description: "Um grupo de astronautas embarca em uma missão para encontrar um novo lar para a humanidade.",
       gender: "Ficção Científica",
       link: "https://www.imdb.com/title/tt0816692/",
-      tags: "interstelar interestrelar"
+      tags: "interstelar interestrelar listar todos"
     },
     {
       title: "O Poderoso Chefão 2",
       description: "A saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos.",
       gender: "Drama, Crime",
       link: "https://www.imdb.com/title/tt0068646/",
-      tags: "chefao cefao o poderoso chefao"
+      tags: "chefao cefao o poderoso chefao listar todos"
     }
   ];
 
